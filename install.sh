23513cbdb1d59990bc7cd7f48cb3b45c896a88b8
Seiji Fouquet <sf@skatekrak.com>
2018-11-15 01:07:22 +0000

    :sparkles: Add config files

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p ~/Library/Application Support/com.fournova.Tower3/CompareTools
+
+cp {CompareTools.plist,vscode.sh} ~/Library/Application Support/com.fournova.Tower3/CompareTools/
