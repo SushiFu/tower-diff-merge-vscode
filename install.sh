41055a8b8a560f44b33f126d72d27158c6dbd289
Seiji Fouquet <sf@skatekrak.com>
2018-11-15 01:14:21 +0000

    :wrench: Add message after install script

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ mkdir -p ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
 
 cp {CompareTools.plist,vscode.sh} ~/Library/Application\ Support/com.fournova.Tower3/CompareTools/
 
+echo "=> Setup completed! Now restart Tower and go to Preferences -> Git Config -> Select 'Visual Studio Code' for both Diff tool and Merge tool."
