fdbf9cd88b6922b17365c34ac2b6bd56b180f1e4
Seiji Fouquet <sf@skatekrak.com>
2018-11-15 01:13:41 +0000

    :bug:  Escape space in paths

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mkdir -p ~/Library/Application Support/com.fournova.Tower3/CompareTools
+mkdir -p ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
 
-cp {CompareTools.plist,vscode.sh} ~/Library/Application Support/com.fournova.Tower3/CompareTools/
+cp {CompareTools.plist,vscode.sh} ~/Library/Application\ Support/com.fournova.Tower3/CompareTools/
+
